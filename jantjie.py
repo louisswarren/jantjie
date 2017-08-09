77bc0b5e1926e205fadaca48718ef98202194ddc
Louis Warren <louis@lsw.nz>
2017-08-09 21:57:37 +1200

    Fix setup

--- jantjie.py
+++ jantjie.py
@@ -63,15 +63,22 @@ def prompt(routine, ps='? '):
 def setup(conn):
     c = conn.cursor()
     c.execute('''
-        CREATE TABLE IF NOT EXISTS 'posts' (
-            'id' INTEGER PRIMARY KEY AUTOINCREMENT,
-            'comment' TEXT,
-            'created' TIMESTAMP DEFAULT CURRENT_TIMESTAMP)
+        CREATE TABLE IF NOT EXISTS posts (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            comment TEXT,
+            created TIMESTAMP DEFAULT CURRENT_TIMESTAMP)
     ''')
+    c.execute("INSERT INTO posts (comment) VALUES ('Hello')")
+    c.execute("INSERT INTO posts (comment) VALUES ('How are you?')")
+    c.execute("INSERT INTO posts (comment) VALUES ('Goodbye')")
     conn.commit()
 
 if __name__ == '__main__':
-    conn = sqlite3.connect('sqlite.db')
-    setup(conn)
+    import os.path
+    if not os.path.exists('sqlite.db'):
+        conn = sqlite3.connect('sqlite.db')
+        setup(conn)
+    else:
+        conn = sqlite3.connect('sqlite.db')
     httpd = http.server.HTTPServer(('', 8000), jantjie_handler(conn))
     httpd.serve_forever()
